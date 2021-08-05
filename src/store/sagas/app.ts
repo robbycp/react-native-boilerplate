910cf00feda386d78a7e82603cb81d9ccab33cbc
robby <dev.robbycaesar@gmail.com>
2021-08-06 05:55:55 +0700

    [MODIFY] RootContext to typesafe

--- src/store/sagas/app.ts
+++ src/store/sagas/app.ts
@@ -3,9 +3,12 @@ import {delay, getContext, put} from 'redux-saga/effects';
 import {authCheckRequest} from '~/store/slices/auth';
 import {appStartCheckSuccess} from '~/store/slices/app';
 import {ScreenName} from '~/types/navigation';
+import {ContextName, RootContext} from '~/store/rootContext';
 
 export function* init() {
-  const navigator = yield getContext('navigator');
+  const navigator: RootContext[ContextName.NAVIGATOR] = yield getContext(
+    ContextName.NAVIGATOR,
+  );
   yield delay(2000);
   // make the call to the api
   yield put(authCheckRequest());
