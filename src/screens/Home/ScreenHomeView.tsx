de46ee0801de053abb3a774a0d74845b5a8b7bb2
robby <dev.robbycaesar@gmail.com>
2021-08-07 01:19:42 +0700

    [ADD] react-native-webview, fix react-native-vector-icons in ios

--- src/screens/Home/ScreenHomeView.tsx
+++ src/screens/Home/ScreenHomeView.tsx
@@ -6,14 +6,12 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
+import {List} from 'react-native-paper';
 
-import {
-  Colors,
-  Header,
-  LearnMoreLinks,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
+import {ScreenHomeViewProps} from './ScreenHomeTypes';
 
-const ScreenHomeView = () => {
+const ScreenHomeView = ({listFeatures}: ScreenHomeViewProps) => {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
@@ -30,7 +28,13 @@ const ScreenHomeView = () => {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <LearnMoreLinks />
+          {listFeatures.map(item => (
+            <List.Item
+              title={item.title}
+              onPress={item.onPress}
+              left={props => <List.Icon {...props} icon={item.icon} />}
+            />
+          ))}
         </View>
       </ScrollView>
     </SafeAreaView>
--- src/types/navigation.ts
+++ src/types/navigation.ts
@@ -1,9 +1,11 @@
 export enum ScreenName {
   HOME = 'home',
   SPLASH = 'splash',
+  WEBVIEW_GOOGLE = 'webview_google',
 }
 
 export type RootStackParamList = {
   [ScreenName.HOME]: undefined;
   [ScreenName.SPLASH]: undefined;
+  [ScreenName.WEBVIEW_GOOGLE]: undefined;
 };
