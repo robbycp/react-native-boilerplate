0d87e39f0b83ac692201849e7fd3564cc9c83eff
robby <dev.robbycaesar@gmail.com>
2021-08-07 23:51:20 +0700

    [ADD] react-native-youtube-iframe

--- src/screens/Home/ScreenHomeView.tsx
+++ src/screens/Home/ScreenHomeView.tsx
@@ -3,13 +3,28 @@ import {
   SafeAreaView,
   ScrollView,
   StatusBar,
+  StyleSheet,
   useColorScheme,
   View,
 } from 'react-native';
-import {Button, List, Text} from 'react-native-paper';
+import {Button, List, Subheading, Text, Title} from 'react-native-paper';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
+import YoutubePlayer from '~/components/basic/YoutubePlayer';
+import {deviceWidth} from '~/utils/calculationView';
 import {ScreenHomeViewProps} from './ScreenHomeTypes';
+
+const marginVideoPlayer = 16;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  videoPlayer: {
+    margin: marginVideoPlayer,
+    width: deviceWidth,
+  },
+});
 
 const ScreenHomeView = ({
   handleShareMessage,
@@ -28,9 +43,19 @@ const ScreenHomeView = ({
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <View>
+        <View style={styles.container}>
+          <Title>Functional Features</Title>
+          <Subheading>Remote Config</Subheading>
           <Text>Firebase Remote Config : {textFirebaseConfig.value}</Text>
+          <Subheading>Share link</Subheading>
           <Button onPress={handleShareMessage}>Share Message</Button>
+          <Subheading>Youtube Player</Subheading>
+          <YoutubePlayer
+            style={styles.videoPlayer}
+            videoId="iee2TATGMyI"
+            width={deviceWidth - 2 * marginVideoPlayer}
+          />
+          <Title>Other Features</Title>
         </View>
         <View
           style={{
