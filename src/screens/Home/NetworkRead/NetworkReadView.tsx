6d1fac65bcd4974ba23de3ca465f93b85e3eab0a
robby <dev.robbycaesar@gmail.com>
2021-08-16 14:20:43 +0700

    [ADD] internationalization with en and id language

--- src/screens/Home/NetworkRead/NetworkReadView.tsx
+++ src/screens/Home/NetworkRead/NetworkReadView.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import {useNetInfo} from '@react-native-community/netinfo';
+import {useTranslation} from 'react-i18next';
 
 const NetworkReadView = () => {
   const netState = useNetInfo();
+  const {t} = useTranslation();
   return (
     <View>
-      <Text>{netState.isConnected ? 'Connected' : 'No connection'}</Text>
+      <Text>
+        {netState.isConnected
+          ? `${t('homeNetwork.connected')}`
+          : `${t('homeNetwork.noConnection')}`}
+      </Text>
     </View>
   );
 };
---
+++ src/translations/id.ts
@@ -0,0 +1,41 @@
+import {Translation} from './en';
+
+const privacy = 'Privasi';
+const image = 'gambar';
+
+const id: Translation = {
+  translation: {
+    common: {
+      image,
+      privacy,
+      loading: 'Memuat',
+    },
+    flatlistImage: {
+      fastImage: 'Gambar Cepat',
+    },
+    home: {
+      authentication: 'Autentikasi',
+      functionalFeatures: 'Fitur Fungsional',
+      firebaseRemoteConfig: 'Konfigurasi Remote Firebase',
+      modalPrivacy: `Modal ${privacy}`,
+      otherFeature: 'Fitur lain',
+      privacy,
+      networkRead: 'Membaca Jaringan',
+      selectLanguage: 'Pilih Bahasa',
+      shareLink: 'Bagikan tautan',
+      shareButton: 'Bagikan pesan',
+      youtubePlayer: 'Pemutar Youtube',
+    },
+    homeClipboard: {
+      clickToCopy: 'Tekan untuk salin',
+      viewCopiedText: 'Lihat text salin',
+      copiedText: 'Salin Halo dunia',
+    },
+    homeNetwork: {
+      connected: 'Connected',
+      noConnection: 'No Connection',
+    },
+  },
+};
+
+export default id;
