c0f169e2832d58fd3d9c8eccfa722dba9edb76a5
robby <dev.robbycaesar@gmail.com>
2021-08-17 13:53:27 +0700

    [MODIFY] screen transition animation prevent error native memoty

--- src/screens/AdsList/AdsListView.tsx
+++ src/screens/AdsList/AdsListView.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import Header from '~/components/basic/Header/Header';
 
 const AdsListView = () => {
   return (
     <View>
+      <Header title="Ads List" />
       <Text>Ads List</Text>
     </View>
   );
