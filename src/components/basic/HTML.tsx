01f8469da7dd01c01151a74e5d1b70b13ffe8d3a
robby <dev.robbycaesar@gmail.com>
2021-08-18 17:11:25 +0700

    [MODIFY] text html based on theme color text

--- src/components/basic/HTML.tsx
+++ src/components/basic/HTML.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import RenderHtml, {HTMLSource} from 'react-native-render-html';
 import {useWindowDimensions} from 'react-native';
+import {useTheme} from 'react-native-paper';
 
 type RenderHtmlProps = React.ComponentProps<typeof RenderHtml>;
 interface Props extends RenderHtmlProps {
@@ -8,8 +9,15 @@ interface Props extends RenderHtmlProps {
 }
 
 const Html = ({source}: Props) => {
+  const theme = useTheme();
   const {width} = useWindowDimensions();
-  return <RenderHtml contentWidth={width} source={source} />;
+  return (
+    <RenderHtml
+      contentWidth={width}
+      source={source}
+      baseStyle={{color: theme.colors.text}}
+    />
+  );
 };
 
 export default Html;
