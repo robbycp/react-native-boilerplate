8f534297f1af3ffe2925b4206f293a082460d47d
robby <dev.robbycaesar@gmail.com>
2021-08-21 17:25:47 +0700

    [ADD] collapsible new screen

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import {Platform} from 'react-native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import ScreenAdsList from '~/screens/AdsList';
+import ScreenCollapsibleBackground from '~/screens/CollapsibleBackground';
+import ScreenCollapsibleCustomHeader from '~/screens/CollapsibleCustomHeader';
+import ScreenCollapsibleDefault from '~/screens/CollapsibleDefault';
+import ScreenCollapsibleSticky from '~/screens/CollapsibleSticky';
+import ScreenCollapsibleSubHeader from '~/screens/CollapsibleSubHeader';
 import ScreenFlatListImage from '~/screens/FlatListImage';
 import ScreenForm from '~/screens/Form';
 import ScreenHome from '~/screens/Home';
@@ -11,7 +17,6 @@ import ScreenSplash from '~/screens/Splash';
 import ScreenWebviewGoogle from '~/screens/WebviewGoogle';
 import {RootStackParamList, ScreenName} from '~/types/navigation';
 import ScreenTermsCondition from '~/screens/T&C';
-import {Platform} from 'react-native';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -22,6 +27,26 @@ const RootNavigator: React.FunctionComponent = () => {
       screenOptions={{headerShown: false}}>
       <Stack.Group screenOptions={{animation: 'slide_from_right'}}>
         <Stack.Screen name={ScreenName.ADS_LIST} component={ScreenAdsList} />
+        <Stack.Screen
+          name={ScreenName.COLLAPSIBLE_BACKGROUND}
+          component={ScreenCollapsibleBackground}
+        />
+        <Stack.Screen
+          name={ScreenName.COLLAPSIBLE_CUSTOM}
+          component={ScreenCollapsibleCustomHeader}
+        />
+        <Stack.Screen
+          name={ScreenName.COLLAPSIBLE_DEFAULT}
+          component={ScreenCollapsibleDefault}
+        />
+        <Stack.Screen
+          name={ScreenName.COLLAPSIBLE_STICKY}
+          component={ScreenCollapsibleSticky}
+        />
+        <Stack.Screen
+          name={ScreenName.COLLAPSIBLE_SUBHEADER}
+          component={ScreenCollapsibleSubHeader}
+        />
         <Stack.Screen
           name={ScreenName.FLATLIST_IMAGE}
           component={ScreenFlatListImage}
---
+++ src/screens/CollapsibleDefault/ScreenCollapsibleDefaultView.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const ScreenCollapsibleDefaultView = () => {
+  return (
+    <View>
+      <Text>Collapsible Default</Text>
+    </View>
+  );
+};
+
+export default ScreenCollapsibleDefaultView;
