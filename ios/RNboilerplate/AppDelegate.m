199e202120ca70462acf4206c38a362884fb689b
robby <dev.robbycaesar@gmail.com>
2021-08-25 06:52:45 +0700

    [ADD] config to build and distribute ota codepush

--- ios/RNboilerplate/AppDelegate.m
+++ ios/RNboilerplate/AppDelegate.m
@@ -6,6 +6,7 @@
 #import <React/RCTLinkingManager.h>
 
 #import <Firebase.h>
+#import <CodePush/CodePush.h>
 
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
@@ -61,7 +62,7 @@ static void InitializeFlipper(UIApplication *application) {
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 #else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  return [CodePush bundleURL];
 #endif
 }
 
