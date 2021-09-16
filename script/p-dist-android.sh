d15c19ec0c263e02cc5b8c03274d06d4d6702099
robby <dev.robbycaesar@gmail.com>
2021-09-16 08:49:37 +0700

    [MODIFY] versionName and versionCode android from npm version, prevent multiple release in sentry, adjust script s-dist and p-dist

--- script/p-dist-android.sh
+++ script/p-dist-android.sh
@@ -1,6 +1,13 @@
+# Update npm version
+prereleaseTag="rc"
+echo "bump prelease production version: $prereleaseTag"
+npm version prerelease --preid $prereleaseTag --no-git-tag-version
+
+# # Build apk
 cd android && ./gradlew assembleRelease
 
+# # Upload apk to appcenter
 uploadFileName=$(find ./app/build/outputs/apk/release/ -type f -iname "*arm64-v8a-release.apk")
 echo "upload file to appcenter: $uploadFileName"
-
 appcenter distribute release --app kudaterbang-test/react-native-boilerplate-android --file "$uploadFileName" --group "Collaborators"
+echo "upload successfull"
--- script/s-dist-android.sh
+++ script/s-dist-android.sh
@@ -1,6 +1,46 @@
+echo "command $1"
+buildType="staging"
+
+# Get last version
+lastVersion=""
+
+if [ "$1" = "release" ]; then
+  buildType="$1"
+  lastVersion=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g')
+  echo "lastVersion: $lastVersion"
+fi
+
+# Update npm version
+branchName=$(git rev-parse --abbrev-ref HEAD)
+branchNameClean=${branchName////-}
+versionName=$branchNameClean
+
+if [ "$buildType" = "release" ]; then
+  versionName="p-$versionName"
+fi
+
+echo "bump prelease staging version: $versionName"
+if ["$buildType" = "release"]
+  then
+    npm version prerelease --preid $versionName --no-git-tag-version
+  else
+    npm version prerelease --preid $versionName
+
+# # Build apk
 cd android && ./gradlew assembleStaging
 
-uploadFileName=$(find ./app/build/outputs/apk/staging/ -type f -iname "*arm64-v8a-staging.apk")
+# # Upload apk to appcenter
+uploadFileName=$(find ./app/build/outputs/apk/$buildType/ -type f -iname "*arm64-v8a-staging.apk")
 echo "upload file to appcenter: $uploadFileName"
-
 appcenter distribute release --app kudaterbang-test/react-native-boilerplate-android --file "${uploadFileName}" --group "Collaborators"
+echo "upload successfull"
+
+# revert npm version for release
+if [ "$buildType" = "release" ]; then
+  npm version $lastVersion --no-git-tag-version
+  echo "revert prelease staging version: $lastVersion"
+fi
