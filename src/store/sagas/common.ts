be6967440f1697deeb3b91f41f3f3a7bcb77e36a
robby <dev.robbycaesar@gmail.com>
2021-09-19 08:00:34 +0700

    [FIX] error linter in slices and sagas

--- src/store/sagas/common.ts
+++ src/store/sagas/common.ts
@@ -1,9 +1,13 @@
 import {call, put} from 'redux-saga/effects';
 import {FirestoreData} from '~/services/firebaseFirestore';
 import Common from '~/services/FirestoreModel/Common';
+import type {PayloadAction} from '@reduxjs/toolkit';
+
+import type {CommonKey} from '~/types/common';
+
 import {commonFetchFailed, commonFetchSuccess} from '../slices/common';
 
-export function* commonFetchSaga(action) {
+export function* commonFetchSaga(action: PayloadAction<{key: CommonKey}>) {
   try {
     const key = action.payload.key;
     const contentData: FirestoreData<{content: string}> = yield call(
--- src/types/common.ts
+++ src/types/common.ts
@@ -2,3 +2,5 @@ export enum CommonData {
   privacyPolicy = 'privacy-policy',
   termsAndCondition = 'terms-and-conditions',
 }
+
+export type CommonKey = CommonData.privacyPolicy | CommonData.termsAndCondition;
