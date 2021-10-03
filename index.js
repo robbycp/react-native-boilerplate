c7fd2dc062a7ac0d88347507f4603b14a7e5a016
robby <dev.robbycaesar@gmail.com>
2021-10-03 08:57:04 +0700

    [ADD] storybook with stories example

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
+import Config from 'react-native-config';
 
-import App from './src';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const AppUsed =
+  Config.LOAD_STORYBOOK === 'true'
+    ? require('./storybook').default
+    : require('./src').default;
+
+AppRegistry.registerComponent(appName, () => AppUsed);
 
 if (__DEV__) {
   require('react-native-performance-flipper-reporter').setupDefaultFlipperReporter();
---
+++ storybook/index.js
@@ -0,0 +1,25 @@
+// if you use expo remove this line
+import {getStorybookUI, configure, addDecorator} from '@storybook/react-native';
+import {withKnobs} from '@storybook/addon-knobs';
+
+import {loadStories} from './storyLoader';
+import './rn-addons';
+
+// enables knobs for all stories
+addDecorator(withKnobs);
+
+// import stories
+configure(() => {
+  loadStories();
+}, module);
+
+// Refer to https://github.com/storybookjs/storybook/tree/master/app/react-native#start-command-parameters
+// To find allowed options for getStorybookUI
+const StorybookUIRoot = getStorybookUI({
+  // host: '192.168.1.101',
+  host: '0.0.0.0',
+  port: '7007',
+  asyncStorage: require('@react-native-async-storage/async-storage').default,
+});
+
+export default StorybookUIRoot;
