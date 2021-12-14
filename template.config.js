7face7332f3fae2e25d276ee735e67a90bcaa860
robby <dev.robbycaesar@gmail.com>
2021-12-14 21:10:32 +0700

    [REMOVE] script in template.config

--- template.config.js
+++ template.config.js
@@ -1,11 +1,4 @@
 module.exports = {
-  // Placeholder used to rename and replace in files
-  // package.json, index.json, android/, ios/
   placeholderName: "RNboilerplate",
-
-  // Directory with template
   templateDir: "./template",
-
-  // Path to script, which will be executed after init
-  postInitScript: "./post-init.script.js"
 };
